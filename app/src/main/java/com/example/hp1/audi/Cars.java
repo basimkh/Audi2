692f9894383357cb6289cc96f463903517cdcc53
basimkh <basim.kharufy@gmail.com>
2017-09-26 11:33:06 +0300

    Initial commit hi hgi

--- app/src/main/java/com/example/hp1/audi/Cars.java
+++ app/src/main/java/com/example/hp1/audi/Cars.java
@@ -2,13 +2,21 @@ package com.example.hp1.audi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class Cars extends AppCompatActivity
+public class Cars extends AppCompatActivity implements View.OnClickListener{
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cars);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
+
